c99091f4b5ce498bc22897f53bd9962ab3836a44
Sudhir Kumar Singh <sudhir.manojsingh@gmail.com>
2023-04-15 15:22:28 +0530

    QZ- rename file and added logic to open add question screen

--- Quizer/src/main/java/com/quizer/servlet/QuizManagerServlet.java
+++ Quizer/src/main/java/com/quizer/servlet/QuizManagerServlet.java
@@ -10,6 +10,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 
 public class QuizManagerServlet extends HttpServlet {
@@ -17,12 +18,20 @@ public class QuizManagerServlet extends HttpServlet {
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String quizName = request.getParameter("quizname");
-		
+
 		if (quizName != null){
 			Quiz quiz = PersistentHelper.singleton.getQuiz(quizName);
-			
+
 			if (quiz == null) {
-				//Proceed Next
+				//Proceed Next with adding question
+				RequestDispatcher rd =request.getRequestDispatcher("addquestion.jsp");
+				quiz = new Quiz();
+				quiz.setName(quizName);
+
+				HttpSession session=request.getSession(true); 
+				session.setAttribute("Quiz", quiz);
+
+				rd.include(request,response);
 			} else {
 				//Show error
 				String errorMessage = String.format("Quiz already exist with %s. Please choose different quiz name.", quizName);
