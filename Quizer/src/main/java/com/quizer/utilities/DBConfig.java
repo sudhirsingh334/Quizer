ded81b990ed8f76b3f1acb21a2b4bf366977fb56
Sudhir Kumar Singh <sudhir.manojsingh@gmail.com>
2023-04-24 18:00:25 +0530

    QZ-1 added dynamic userName and password of bd

--- Quizer/src/main/java/com/quizer/utilities/DBConfig.java
+++ Quizer/src/main/java/com/quizer/utilities/DBConfig.java
@@ -1,9 +1,11 @@
 package com.quizer.utilities;
 
 public class DBConfig {
+public static String url = "jdbc:mysql://localhost:3306/quizer";
+	//public static String username = "root";
+	//public static String password = "Sudhirk@9934";
 
-	public static String url = "jdbc:mysql://localhost:3306/quizer";
-	public static String username = "root";
-	public static String password = "Sudhirk@9934";
+public static String username = "sudhirk";
+public static String password = "sudhirk";
 	
 }
