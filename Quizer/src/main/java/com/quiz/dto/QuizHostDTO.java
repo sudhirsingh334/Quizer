275764b4590a4d424ad5e9e3ccd1e77591cb3cd7
Sudhir Kumar Singh <sudhir.manojsingh@gmail.com>
2023-04-30 13:57:23 +0530

    QZ-1: adde dnew property in in the DTO

--- Quizer/src/main/java/com/quiz/dto/QuizHostDTO.java
+++ Quizer/src/main/java/com/quiz/dto/QuizHostDTO.java
@@ -7,38 +7,65 @@ import com.database.QuizHostState;
 public class QuizHostDTO {
 	private String id;
 	private String code;
+	private String quizId;
+	private String quizName;
 	private QuizHostState status;
 	private String hostedAt;
 	private ArrayList<ResultCandidate> candidateList;
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getCode() {
 		return code;
 	}
+
 	public void setCode(String code) {
 		this.code = code;
 	}
+	
+	public String getQuizId() {
+		return quizId;
+	}
+
+	public void setQuizId(String quizId) {
+		this.quizId = quizId;
+	}
+
+	public String getQuizName() {
+		return quizName;
+	}
+
+	public void setQuizName(String quizName) {
+		this.quizName = quizName;
+	}
+
 	public QuizHostState getStatus() {
 		return status;
 	}
+
 	public void setStatus(QuizHostState status) {
 		this.status = status;
 	}
+
 	public String getHostedAt() {
 		return hostedAt;
 	}
+
 	public void setHostedAt(String hostedAt) {
 		this.hostedAt = hostedAt;
 	}
+
 	public ArrayList<ResultCandidate> getCandidateList() {
 		return candidateList;
 	}
+
 	public void setCandidateList(ArrayList<ResultCandidate> candidateList) {
 		this.candidateList = candidateList;
 	}
-
 }
